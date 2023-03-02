a331819bd52004be45b21fb7afe061a7d7ab509a
VAGHASIYA6733 <vaghasiyaakshit.673@gmail.com>
2023-03-02 11:44:03 +0530

    change 1

--- para.js
+++ para.js
@@ -10,11 +10,10 @@ function data1 (){
       function replace(){
         const fname1 = document.getElementById("text").value  
         document.getElementById("dataa").innerHTML=fname1    
-        const p = document.getElementById("input1").value
-        const p1 = document.getElementById("text").value  
+        const p = document.getElementById("input2").value
+        const p1 = document.getElementById("input1").value
         document.getElementById("datap").innerHTML=
-        p1.replaceAll('dog', p);
+        fname1.replaceAll(p1, p);
 // Expected output: "The quick brown fox jumps over the lazy monkey. If the monkey reacted, was it really lazy?"
-
 
       }
