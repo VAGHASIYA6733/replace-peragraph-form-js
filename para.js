31ebcc5adbd437b4a13405956a1be092ed31daf9
VAGHASIYA6733 <vaghasiyaakshit.673@gmail.com>
2023-03-02 10:38:12 +0530

    start project

---
+++ para.js
@@ -0,0 +1,20 @@
+function data1 (){
+
+    }
+    function handleSubmit(event) {
+        // document.getElementById("myForm").reset();
+        event.preventDefault();
+       replace();
+      }
+    
+      function replace(){
+        const fname1 = document.getElementById("text").value  
+        document.getElementById("dataa").innerHTML=fname1    
+        const p = document.getElementById("input1").value
+        const p1 = document.getElementById("text").value  
+        document.getElementById("datap").innerHTML=
+        p1.replaceAll('dog', p);
+// Expected output: "The quick brown fox jumps over the lazy monkey. If the monkey reacted, was it really lazy?"
+
+
+      }
